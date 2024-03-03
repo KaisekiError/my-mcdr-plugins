be0f413d940a9cc154530425eae8d8582d20ef2f
kaisekierror <1282801373@qq.com>
2024-03-03 14:31:48 +0800

    bot识别

--- qq_bot/qq_bot/__init__.py
+++ qq_bot/qq_bot/__init__.py
@@ -1,8 +1,9 @@
-from mcdreforged.api.types import PluginServerInterface
+from mcdreforged.api.types import PluginServerInterface, Info
 from mcdreforged.api.utils import Serializable
 from mcdreforged.command.builder.nodes.arguments import GreedyText
 from mcdreforged.command.builder.nodes.basic import Literal
-from mcdreforged.info_reactor.info import Info
+# from mcdreforged.info_reactor.info import Info
+from parse import parse
 from aiocqhttp import CQHttp, Event
 from asyncio import AbstractEventLoop
 from qq_api import MessageEvent
@@ -21,10 +22,11 @@ user_cache: dict
 final_bot: CQHttp
 event_loop: AbstractEventLoop
 group: int
+true_players = ()
 
 
 def on_load(server: PluginServerInterface, prev):
-    global config, data, final_bot, event_loop, group
+    global config, data, final_bot, event_loop, group, true_players
     server.logger.info(f'{Config.server_name} - MCDR服务端运行中...')
     qq_api = server.get_plugin_instance("qq_api")
     final_bot = qq_api.get_bot()
@@ -49,12 +51,16 @@ def on_load(server: PluginServerInterface, prev):
 
 
 def on_server_startup(server: PluginServerInterface):
+    global true_players
+    true_players = ()
     server.logger.info(f'{config.server_name} 游戏服务器已启动')
     send_msg(f'{config.server_name} 游戏服务器已启动! わくわくになっちゃうね!')
 
 
 def on_server_stop(server: PluginServerInterface, server_return_code: int):
+    global true_players
     server.logger.info(f'{config.server_name} 游戏服务器已停止')
+    true_players = ()
     if server_return_code == 0:
         send_msg(f'{config.server_name} 游戏服务器已停止...到了休眠的时间了呢')
     else:
@@ -62,31 +68,50 @@ def on_server_stop(server: PluginServerInterface, server_return_code: int):
 
 
 def on_player_joined(server: PluginServerInterface, player: str, info: Info):
-    send_msg(f'{player} 加入了 {config.server_name} 服务器! 好耶!')
+    global true_players
+    if info.is_from_server:
+        psd = parse('{name}[{ip}] logged in with entity id {id} at {loc}', info.content)
+        if psd and psd['ip'] != 'local':
+            true_players.add(psd['name'])
+            send_msg(f'{player} 加入了 {config.server_name} 服务器! 好耶!')
 
 
 def on_player_left(server: PluginServerInterface, player: str):
-    send_msg(f'{player} 退出了 {config.server_name} 服务器! 呜...')
+    global true_players
+    if player in true_players:
+        send_msg(f'{player} 退出了 {config.server_name} 服务器! 呜...')
+        true_players.remove(player)
 
 
 def on_message(server: PluginServerInterface, bot: CQHttp,
                event: MessageEvent):  # qq群聊向minecraft发送消息
+    global true_players
     raw_message = event.message
     user_id = event.sender['nickname']
-    if re.match('^!!mc .*', str(raw_message)):    # !!mc指令
+    if re.match('^!!mc .*', str(raw_message)):  # !!mc指令
         processed_message = re.sub(r'!!mc\s*(.*)', r'\1', str(raw_message))
         server.logger.info(f'[QQ]§e{user_id} : {processed_message}')
         server.say(f'§e[QQ] {user_id} : {processed_message}')
-    elif re.match('^!!list', str(raw_message)):    # !!list指令
+    elif re.match('^!!list', str(raw_message)):  # !!list指令
         players = get_player_list()
         player_count = len(get_player_list())
-        send_msg("{server_name}服务器目前共有{player_count}名玩家:{players}".format(
-            server_name=config.server_name, player_count=player_count,
-            players=str(players).replace('[', '').replace(']', '')))
+        send_msg("{server_name}服务器目前共有{all_player_count}名玩家:{all_players}, 其中有{true_count}位真人:{true_players}".format(
+            server_name=config.server_name, all_player_count=player_count,
+            all_players=str(players).replace('[', '').replace(']', ''),
+            true_count=len(true_players),
+            true_players=str(true_players).replace(r'(', '').replace(r')', '')))
     elif raw_message.startswith('/'):
         is_command = True
     else:
         pass
+
+
+# def on_info(server: PluginServerInterface, info: Info):
+#     global true_players
+#     if info.is_from_server:
+#         psd = parse('{name}[{ip}] logged in with entity id {id} at {loc}', info.content)
+#         if psd and psd['ip'] != 'local':
+#             true_player.add(psd['name'])
 
 
 def on_notice(server: PluginServerInterface, bot: CQHttp, event: Event):
